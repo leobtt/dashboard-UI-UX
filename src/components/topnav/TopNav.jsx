2cd0bb0aed09012a1b32533044fc96627cf04a97
leobtt <leonarbat8@gmail.com>
2022-02-09 10:58:22 -0300

    topnav

--- src/components/topnav/TopNav.jsx
+++ src/components/topnav/TopNav.jsx
@@ -1,8 +1,16 @@
+import React from 'react'
+import './topnav.scss'
+import data from '../../constants/data'
+import UserInfo from './UserInfo'
+
 const TopNav = () => {
   return (
-    <>
-      <p>Topnav</p>
-    </>
+    <div className="topnav">
+      <UserInfo user={data.user} />
+      <div className="sidebar-toggle">
+        <i className="bx bx-menu-alt-right"></i>
+      </div>
+    </div>
   )
 }
 
