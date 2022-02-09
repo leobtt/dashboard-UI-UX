9e5c929316390fa476f0f86dc160ee193e03db83
leobtt <leonarbat8@gmail.com>
2022-02-09 10:05:06 -0300

    sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './sidebar.scss'
+import { Link, useLocation } from 'react-router-dom'
+import images from '../../constants/images'
+import sidebarNav from '../../configs/sidebarNav'
 
 const Sidebar = () => {
+  const [activeIndex, setActiveIndex] = useState(0)
+  const location = useLocation()
+
+  useEffect(() => {
+    const curPath = window.location.pathname.split('/')[1]
+    const activeItem = sidebarNav.findIndex((item) => item.section === curPath)
+
+    setActiveIndex(curPath.length === 0 ? 0 : activeItem)
+  }, [location])
+
   return (
     <>
-      <p>Sidebar</p>
+      <div className="sidebar">
+        <div className="sidebar__logo">
+          <img src={images.logo} alt="usuário" />
+          <div className="sidebar-close">
+            <i className="bx bx-x bx-flashing-hover">hello</i>
+          </div>
+        </div>
+        <div className="sidebar__menu">
+          {sidebarNav.map((nav, index) => (
+            <Link
+              to={nav.link}
+              key={`nav-${index}`}
+              className={`sidebar__menu__item ${
+                activeIndex === index && 'active'
+              }`}
+            >
+              <div className="sidebar__menu__item__icon">{nav.icon}</div>
+              <div className="sidebar__menu__item__txt">{nav.text}</div>
+            </Link>
+          ))}
+
+          <div className="sidebar__menu__item">
+            <div className="sidebar__menu__item__icon">
+              <i className="bx bx-log-out"></i>
+            </div>
+            <div className="sidebar__menu__item__txt">Logout</div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
